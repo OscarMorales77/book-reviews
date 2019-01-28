894d1e465d4501ed622c8324a7a6e8dee282d76d
Oscar <NAge@email.com>
2019-01-27 19:12:24 -0600

    Logic for registration, login,logout,import, and search are complete!

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 import os
-import csv
 
 from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+# import importBooks2
 app = Flask(__name__)
 
 # Check for environment variable
@@ -21,20 +21,72 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-#A global variable for the current session to track user
-current_user=None
+# A global variable for the current session to track user
+current_user = None
+
 
 @app.route("/")
 def index():
-
+    # importBooks2.some_fun()
     return render_template("index.html")
 
 
-@app.route("/hello", methods=["POST"])
+def query_results(query, m_type):
+    if m_type == "isbn" or m_type == "author":
+        print("-------calling isbn/author function " +m_type)
+        #something to consider is the performance of LOWER on a large database
+        values = db.execute(f"select * from books where LOWER({m_type}) like LOWER('{query}%')").fetchall()
+        if len(values) == 0:
+            return "Sorry no Results!"
+
+        results = ""
+        for row in values:
+            # different methods to get columns  either by "[i]" or the ".colName"
+            results = results + "\n" + f"{row[0]} | {row[1]} | {row.author} | {row.year}"
+
+        return results
+    else:
+        print("-------calling author function " +m_type)
+        values = db.execute(f"select * from books where title  ~* '{query}' ").fetchall()
+        print(len(values))
+        if len(values) == 0:
+            return "Sorry no Results!"
+
+        results = ""
+        for row in values:
+            # different methods to get columns  either by "[i]" or the ".colName"
+            results = results + "\n" + f"{row[0]} | {row[1]} | {row.author} | {row.year}"
+
+        return results
+
+
+@app.route("/search", methods=["POST", "GET"])
+def search():
+    if request.method == "POST":
+        isbn = request.form.get("isbn")
+        title = request.form.get("title")
+        author = request.form.get("author")
+        if isbn is not None and len(isbn) != 0:
+
+            return query_results(isbn, "isbn")
+        elif title is not None and len(title) != 0:
+
+            return query_results(title, "title")
+        elif author is not None and len(author) != 0:
+
+            return query_results(author, "author")
+        else:
+            return "No Results, sorry!"
+
+    return render_template("search.html")
+
+
+@app.route("/register", methods=["POST"])
 def hello():
     first = request.form.get("firstName")
     last = request.form.get("lastName")
-    ''' This will be saved to the DataBase'''
+    myMap = {"first": first, "last": last}
+    db.execute("INSERT INTO users(username, password) VALUES  (:first, :last)", myMap)
     return "You typed " + first + " " + last
 
 
@@ -42,24 +94,28 @@ def hello():
 def verify():
     username = request.form.get("username")
     password = request.form.get("password")
-
-    if password == "password" and username == "username":
+    sql_command = f"select username, password from users where LOWER(username)=LOWER('{username}') AND LOWER(password)=LOWER('{password}')"
+    values = db.execute(sql_command).fetchall()
+    # values is a "list/array" of sqlAl objects
+    # each sqlAlchemy object can be accessed with [i]
+    # actually, I don't need username and password because the sql query is already performing those boolean checks
+    if len(values) == 1 and username == values[0][0] and password == values[0][1]:
         session[username] = True
         return "You are logged in!"
 
-    return "Not registered!"
+    return "You are not registered"
 
 
 @app.route("/secret")
 def secret():
-    for key in session.items():
-        print(key)
-    if len(session) != 0 and session["username"]:
+    # global variable is the "session" that is a map/dict, i'm using the list function to get a list of the keys in the map
+    # as that is the only way one can index the keys; the register maps the username to a boolean true which i check below
+    if len(session) != 0 and session[list(session)[0]]:
         return "Secrete Page"
     return "Not logged-in!"
 
 
 @app.route("/logout", methods=["POST"])
 def logout():
-    session.clear()
+    session.clear()  # remove all keys from the map named "session" that is a global variable
     return "logged out!"
--- importBooks2.py
+++ importBooks2.py
@@ -8,11 +8,10 @@ if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
 engine = create_engine(os.getenv("DATABASE_URL"))
-
 db = scoped_session(sessionmaker(bind=engine))
 
 
-def main():
+def mainN():
     f = open("books.csv")
 
     reader = csv.reader(f, doublequote=True, quoting=csv.QUOTE_ALL, escapechar='\\')
@@ -22,6 +21,30 @@ def main():
         db.execute("INSERT INTO books(isbn, title, author, year) VALUES  (:isbn, :title, :author, :year)", myMap)
         db.commit()
 
+def mainOther():
+    username="BOb"
+    password="PAsS1v"
+    sql_command=f"select username, password from users where LOWER(username)=LOWER('{username}') AND LOWER(password)=LOWER('{password}')"
+    values = db.execute(sql_command).fetchall()
+    print("the size of values is " + str(len(values)))
+    print(values[0][0])
+    print(values[0][1])
 
+def main():
+    values=db.execute("select * from books where title  ~* 'dEAd' limit 7").fetchall()
+    #values2=db.execute("select * from books where LOWER(title) like LOWER('%dead%') ").fetchall()
+    results=""
+    for row in values:
+        results=results+"\n"+f"{row[0]} | {row[1]} | {row.author} | {row.year}"
+        #print(f"{row[0]}% --{row[1]}---{row.author}--{row.year}")
+
+    print(results)
+def some_fun():
+    return print("----------yheeee--------")
+
+def mainGGG():
+    query=185798809488
+    values = db.execute(f"select * from books where isbn like '{query}%' ").fetchall()
+    print(len(values))
 if __name__ == "__main__":
     main()
